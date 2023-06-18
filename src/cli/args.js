1b78c5f23012d6a3b5af22950d40ec599105404c
Melnychuk <imelnychuk@luxoft.com>
2023-06-19 00:31:27 +0200

    feat: implement functions in src/cli

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,17 @@
+const prefix = '--';
+
 const parseArgs = () => {
-    // Write your code here 
+    const argsArr = process.argv.slice(2);
+    const argsStr = argsArr.reduce((acc, arg, index, arr) => {
+        if (arg.startsWith(prefix)) {
+            const valueStr = `${arg.replace(prefix, '')} is ${arr[index + 1]}`;
+            return [...acc, valueStr];
+        }
+        return acc;
+    }, [])
+    .join(', ');
+
+    console.log(argsStr);
 };
 
 parseArgs();
