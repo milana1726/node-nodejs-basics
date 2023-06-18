01c1db5b6f9e5badb78d31969492719f519c82be
Melnychuk <imelnychuk@luxoft.com>
2023-06-18 18:23:27 +0200

    feat: implement functions 'existsAsync' and 'create'

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,20 @@
+import { writeFile } from 'fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { existsAsync } from '../utils/existsAsync.js';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const filePath = path.join(__dirname, 'files', 'fresh.txt');
+
 const create = async () => {
-    // Write your code here 
+    const fileExists = await existsAsync(filePath);
+
+    if (fileExists) {
+        throw new Error('FS operation failed');
+    } else {
+        await writeFile(filePath, 'I am fresh and young');
+        console.log('File created successfully!')
+    };
 };
 
 await create();
