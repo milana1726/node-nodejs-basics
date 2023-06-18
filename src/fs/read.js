5beb285a6405e8f1b6cd39c483f17c455908e66f
Melnychuk <imelnychuk@luxoft.com>
2023-06-18 18:28:30 +0200

    feat: implement function 'read'

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,17 @@
+import { readFile } from 'fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const filePath = path.join(__dirname, 'files', 'fileToRead.txt');
+
 const read = async () => {
-    // Write your code here 
+    try {
+        const fileContent = await readFile(filePath, 'utf-8');
+        console.log(fileContent);
+    } catch {
+        throw new Error('FS operation failed');
+    }
 };
 
 await read();
