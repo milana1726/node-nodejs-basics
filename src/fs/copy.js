b005039614b6422c321950206a76739ee8a5802d
Melnychuk <imelnychuk@luxoft.com>
2023-06-18 18:24:16 +0200

    feat: implement function 'copy'

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,19 @@
+import { cp } from 'fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { existsAsync } from '../utils/existsAsync.js';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const sourceFolderPath = path.join(__dirname, 'files');
+const copyFolderPath = path.join(__dirname, 'files_copy');
+
 const copy = async () => {
-    // Write your code here 
+    if (!(await existsAsync(sourceFolderPath)) || await existsAsync(copyFolderPath)) {
+        throw new Error('FS operation failed');
+    } else {
+        await cp(sourceFolderPath, copyFolderPath, { recursive: true });
+        console.log('Copy created successfully!')
+    };
 };
 
 await copy();
