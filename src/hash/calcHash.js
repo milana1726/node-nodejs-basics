ceba5f6601ba7b452740dd2665e6f6ba7e90183e
Melnychuk <imelnychuk@luxoft.com>
2023-06-19 00:36:56 +0200

    feat: implement function 'calcHash'

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,16 @@
+import { createHash } from 'crypto';
+import { readFile } from 'fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const filePath = path.join(__dirname, 'files', 'fileToCalculateHashFor.txt');
+
 const calculateHash = async () => {
-    // Write your code here 
+    const fileContent = await readFile(filePath);
+    const hash = createHash('sha256').update(fileContent).digest('hex');
+
+    console.log(hash);
 };
 
 await calculateHash();
