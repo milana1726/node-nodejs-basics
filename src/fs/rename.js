f5d3b2990934facf1bb453d5e7f44a6114fa983f
Melnychuk <imelnychuk@luxoft.com>
2023-06-18 18:25:23 +0200

    feat: implement function 'rename'

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,21 @@
+import { rename as renameFile } from 'fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { existsAsync } from '../utils/existsAsync.js';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const sourceFilePath = path.join(__dirname, 'files', 'wrongFilename.txt');
+const newFilePath = path.join(__dirname, 'files', 'properFilename.md' );
+
 const rename = async () => {
-    // Write your code here 
+    const fileExists = await existsAsync(newFilePath);
+
+    if (fileExists) {
+        throw new Error('FS operation failed');
+    } else {
+        await renameFile(sourceFilePath, newFilePath);
+        console.log('File renamed successfully!');
+    };
 };
 
 await rename();
