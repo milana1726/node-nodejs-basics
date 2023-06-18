8204533883ba2a217e9072f6f39d37ebd81e5e9f
Melnychuk <imelnychuk@luxoft.com>
2023-06-18 18:26:02 +0200

    feat: implement function 'delete'

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,17 @@
+import { rm } from 'fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const sourceFilePath = path.join(__dirname, 'files', 'fileToRemove.txt' );
+
 const remove = async () => {
-    // Write your code here 
+    try {
+        await rm(sourceFilePath);
+        console.log('File deleted successfully!')
+    } catch {
+        throw new Error('FS operation failed');
+    }
 };
 
 await remove();
