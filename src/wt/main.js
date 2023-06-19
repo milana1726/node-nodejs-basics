9b6e69f950953dc82e5892c778888a3dcf546298
Melnychuk <imelnychuk@luxoft.com>
2023-06-19 23:10:30 +0200

    feat: implement functions in src/wt

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,27 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { Worker } from 'worker_threads';
+import { cpus } from 'os';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const workerPath = path.join(__dirname, 'worker.js');
+
 const performCalculations = async () => {
-    // Write your code here
+    let promises = [];
+
+    for (let i = 0; i < cpus().length; i++) {
+        const res = new Promise((resolve, reject) => {
+            const worker = new Worker(workerPath, { workerData: i + 10 });
+
+            worker.on('message', (data) => resolve({ status: 'resolved', data: data }));
+            worker.on('error', () => resolve({ status: 'error', data: null }));
+        });
+
+        promises.push(res);
+    }
+
+    Promise.all(promises).then((data) => console.log(data));
 };
 
 await performCalculations();
